a2f8742796bff2b74a22e4f12f5e3c6b417c233c
Marianni <ana.marianni@netsoft.com>
2026-01-28 19:02:31 -0300

    criacao das paginas de login e perfil candidato

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const waitlistRoutes = require('./routes/waitlist.routes');
+const usersRoutes = require('./routes/users.routes');
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/waitlist', waitlistRoutes);
+app.use('/users', usersRoutes);
 
 module.exports = app;
